2ed8f5d46172a24787bb43d0d0c036781791140c
owos02 <76398608+owos02@users.noreply.github.com>
2026-01-07 12:58:33 +0100

    fix info message for script in edgecase

--- scripts/setup_needed_libraries.sh
+++ scripts/setup_needed_libraries.sh
@@ -1,12 +1,19 @@
 #! /bin/bash
 clear
 
+readonly NEEDED_DLLS=("Assembly-CSharp.dll" "UnityEngine.dll" "UnityEngine.CoreModule.dll" "UnityEngine.IMGUIModule.dll" "UnityEngine.TextRenderingModule.dll" )
+
 readonly echo_default_color='\033[0m'
 readonly echo_error_color='\033[0;31m'
 readonly echo_success_color='\033[0;92m'
 readonly echo_note_color='\033[0;37m'
 readonly echo_info_color='\033[0;33m'
 readonly echo_file_highlight_color='\033[1;4;97m'
+
+# Game Folder/s
+readonly GAME_FOLDER='.local/share/Steam/steamapps/common/Tails of Iron/TOI_Data/Managed'
+# Dev Folder
+readonly PLUGIN_LIB_FOLDER='lib'
 
 echo -e -n "${echo_note_color}Executed script in correct directory? "
 readonly WORKING_DIRECTORY=$(pwd)
@@ -18,24 +25,19 @@ if [ "$CURRENT_FOLDER" != "$DEVELOPER_DIRECTORY_NAME" ]; then
   echo -e "${echo_error_color}NO\nWrong directory"
   echo -e "${echo_info_color}Run the script in ${echo_file_highlight_color}ROOT directory${echo_info_color}. If it still fails continue reading!\n"
   echo -e "${echo_note_color}Script must be run in the root folder, and if you did\nthe directory name of the plugin was changed!\n"
-  echo -e "${echo_note_color}To fix this go into the script and change the name in ${echo_file_highlight_color}line: 10\n"
+  echo -e "${echo_note_color}To fix this go into the script \"setup_needed_libraries.sh\" and change the name in ${echo_file_highlight_color}line: 21\n"
   
   exit 1
 fi
 echo -e "${echo_success_color}YES${echo_note_color}"
 
 echo -e -n "${echo_note_color}Does the lib folder exist? "
-if [ ! -d "$WORKING_DIRECTORY/lib" ]; then
+if [ ! -d "$WORKING_DIRECTORY/$PLUGIN_LIB_FOLDER" ]; then
   echo -e "${echo_info_color}No\nCreating folder ${echo_file_highlight_color}${WORKING_DIRECTORY}/lib${echo_note_color}"
-  mkdir "$WORKING_DIRECTORY/lib"
+  mkdir "$WORKING_DIRECTORY/$PLUGIN_LIB_FOLDER"
 else
-  echo -e "${echo_info_color}YES${echo_note_color}"
+  echo -e "${echo_success_color}YES${echo_note_color}"
 fi
-# Game Folder/s
-readonly GAME_FOLDER='.local/share/Steam/steamapps/common/Tails of Iron/TOI_Data/Managed'
-readonly NEEDED_DLLS=("Assembly-CSharp.dll" "UnityEngine.dll" "UnityEngine.CoreModule.dll" "UnityEngine.IMGUIModule.dll" "UnityEngine.TextRenderingModule.dll" )
-# Dev Folder
-readonly PLUGIN_LIB_FOLDER='lib'
 
 echo -n "Does folder $HOME/$GAME_FOLDER exist? "
 if [ ! -d "$HOME/$GAME_FOLDER" ]; then
