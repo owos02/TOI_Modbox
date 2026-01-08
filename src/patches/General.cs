f89d647a0864b7459214d74f98b44bf3f08ca314
owos02 <76398608+owos02@users.noreply.github.com>
2026-01-08 10:39:36 +0100

    hide inconsistentNaming warnings

--- src/patches/General.cs
+++ src/patches/General.cs
@@ -3,12 +3,14 @@ namespace com.owos02.toi_modbox;
 using HarmonyLib;
 
 internal partial class TOIPatches {
+    // ReSharper disable once InconsistentNaming
     [HarmonyPatch(typeof(SplashScreen), "ShowScreen")]
     [HarmonyPrefix]
     public static bool Patch_SkipSplashScreens(ref bool __result) {
         return __result = !Plugin.settings.configSplashScreenSkip.Value;
     }
 
+    // ReSharper disable once InconsistentNaming
     [HarmonyPatch(typeof(Player), "ManagedOnDestroy")]
     [HarmonyPrefix]
     public static void Patch_UnloadSaveDataFromGUI(Player __instance) {
@@ -18,6 +20,7 @@ internal partial class TOIPatches {
         Settings.saveFile = Settings.NO_SAVE_FILE_SELECTED;
     }
 
+    // ReSharper disable once InconsistentNaming
     [HarmonyPatch(typeof(SaveDataManager), "OnSaveFileLoadComplete")]
     [HarmonyPostfix]
     public static void Patch_GetSaveSlotData(object __instance) {
